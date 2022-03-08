ef28cb690e4bd7b6501817a9c54cfe6f43dfe707
giorginogreg <giorgino.greg@gmail.com>
2022-03-08 13:37:12 +0100

    Added references concepts, aliasing and pointers

--- hello_world/main.cpp
+++ hello_world/main.cpp
@@ -1,5 +1,5 @@
 using namespace std;
-#include <iostream>
+#include <iostream> // simile alla Forward declaration
 
 int main(int argc, char const *argv[])
 {
@@ -8,6 +8,41 @@ int main(int argc, char const *argv[])
 
     string name;
     cin >> name;
-    cout << "Hello " << name  << "!" << endl;    
+    cout << "Hello " << name  << "!" << endl;   
+
+
+    // Reference example
+    int val = 10;
+    int* address_val = &val;
+
+    // I can dereference a pointer, not a REFERENCE!
+    cout << "Value of val: " << *address_val  << endl;   
+
+    // Pointer can change their location address, while references CAN'T
+
+    int& val_pointer = val; // Initialization mandatory on creation.
+    
+    // null pointer example
+    // Class null_pointer = nullptr;
+
+    // Aliases (using T = Y) = typedef
+    using intero = int;
+    typedef int intero;
+
+    // Decltype
+    // It deduces the value type but doesn't evaluate it (example for functions)
+
+    int a = 1;
+    int b = 2;
+    swap(a, b);
+    cout << "Value of A and B are " << to_string(a) << " | " << to_string(b) << endl;
+
     return 0;
+}
+
+// Example of function with parameters passed by reference.
+void swap(int& v1, int& v2) {
+    int tmp = v2;
+    v2 = v1;
+    v1 = tmp;
 }
