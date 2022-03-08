9402b329e225c17aecbd24573266eb10646e1ee3
giorginogreg <giorgino.greg@gmail.com>
2022-03-08 12:31:10 +0100

    Added hello world

---
+++ hello_world/main.cpp
@@ -0,0 +1,13 @@
+using namespace std;
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    cout << "Hello World!" << endl;    
+    cout <<  "What's your name?"  << endl;    
+
+    string name;
+    cin >> name;
+    cout << "Hello " << name  << "!" << endl;    
+    return 0;
+}
