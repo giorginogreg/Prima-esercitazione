89980708730b6522f0f6b922ea0ea59c5786f763
giorginogreg <giorgino.greg@gmail.com>
2022-03-22 12:27:37 +0100

    Removed node elem

--- TDA_by_greg/Lists/src/Node.cpp
+++
@@ -1,8 +0,0 @@
-#include "../include/Node.h"
-
-template <typename T>
-TDA_Greg::Node<T>::Node(T elem)
-{
-    _elem = elem;
-    _nextPos = nullptr; //TODO: secondo me da fare astratto con metodo da implementare
-}
