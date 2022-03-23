57e7917fda55cd92b34f86fa9c70de7d950778e5
giorginogreg <giorgino.greg@gmail.com>
2022-03-23 11:29:24 +0100

    Stack list implementation (to test)

--- TDA_by_greg/Lists/pointer_implementation/include/StackList.h
+++ TDA_by_greg/Lists/pointer_implementation/include/StackList.h
@@ -6,15 +6,50 @@
 #define ALGORITHM_DATA_STRUCTURES_STACKLIST_H
 
 #include "../../include/LinearList.h"
+#include "../include/LinkedList.h"
 #include "Node.h"
 template<class T, int S>
 class StackList: public LinearList<T, TDA_Greg::Node<T> *, S> {
 
+public:
     void push();
     void pop(); // Discard last changes
     bool empty();
     T top(); // Return last inserted element
 
+private:
+    LinkedList<T, S> _l;
+    TDA_Greg::Node<T> getLastNode();
 };
+
+template<class T, int S>
+void StackList<T, S>::push() {
+    auto last_node = getLastNode();
+    _l.insertNodeAfter(last_node);
+}
+
+template<class T, int S>
+TDA_Greg::Node<T> StackList<T, S>::getLastNode() {
+    auto first_node = _l.firstNodeList();
+    while(!_l.isLastPosition(first_node)) first_node = first_node->getNextPos();
+    return first_node;
+}
+
+template<class T, int S>
+void StackList<T, S>::pop() {
+    auto last_node = getLastNode();
+    _l.deleteNodeAt(last_node);
+}
+
+template<class T, int S>
+bool StackList<T, S>::empty() {
+    return _l.isEmpty();
+}
+
+template<class T, int S>
+T StackList<T, S>::top() {
+    return getLastNode().getElem();
+}
+
 
 #endif //ALGORITHM_DATA_STRUCTURES_STACKLIST_H
